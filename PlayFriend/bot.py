c80f96001b8abb6285ac49bda790d323f31299e1
lbcai <73155687+lbcai@users.noreply.github.com>
2020-12-24 23:47:12 -0800

    Added basic startup message and ability to respond to messages.

--- PlayFriend/bot.py
+++ PlayFriend/bot.py
@@ -8,10 +8,27 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    ready_message = f'{client.user.name} is here! Type ">help" for a basic command list.'
+    channel = discord.utils.get(client.get_all_channels(), name='playfriend')
+    await channel.send(ready_message)
+
+
+@client.event
+async def on_message(message):
+    print('message received')
+    if message.author == client.user:
+        return
+
+    help_command_list = '>help: Returns a list of basic commands. \n>test'
+
+    if message.content == '>help':
+        await message.channel.send(help_command_list)
+
 
 client.run(TOKEN)
 
-# Thanks to RealPython.com for this code.
+# Thanks to RealPython.com for tutorials on the basics of Discord bot connection.
