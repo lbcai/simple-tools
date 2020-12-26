0ba1c1b7daa1a0e25b5c3f8258859bda585bd8d4
lbcai <73155687+lbcai@users.noreply.github.com>
2020-12-25 23:59:24 -0800

    Testing hangman images.

--- PlayFriend/bot.py
+++ PlayFriend/bot.py
@@ -19,14 +19,82 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print('message received')
     if message.author == client.user:
         return
 
-    help_command_list = '>help: Returns a list of basic commands. \n>test'
+    help_command_list = '>help: Returns a list of basic commands. \n>hangman: Start a game of hangman.'
 
-    if message.content == '>help':
+    if message.content.lower() == '>help':
         await message.channel.send(help_command_list)
+
+    hangman_full = (f'```\n _________\n'
+                    f'|/      _|_\n'
+                    r"|      |x.x|"
+                    f'\n'
+                    r'|      /[Y]\ '
+                    f'\n'
+                    r'|       / \ '
+                    f'\n'
+                    f'|_____```')
+
+    hangman1 = (f'```\n _________\n'
+                f'|/      _|_\n'
+                r"|      |'-'|"
+                f'\n'
+                r'|      '
+                f'\n'
+                r'|      '
+                f'\n'
+                f'|_____```')
+
+    hangman2 = (f'```\n _________\n'
+                f'|/      _|_\n'
+                r"|      |'-'|"
+                f'\n'
+                r'|       [Y] '
+                f'\n'
+                r'|       '
+                f'\n'
+                f'|_____```')
+
+    hangman3 = (f'```\n _________\n'
+                f'|/      _|_\n'
+                r"|      |'-'|"
+                f'\n'
+                r'|      /[Y] '
+                f'\n'
+                r'|       '
+                f'\n'
+                f'|_____```')
+
+    hangman4 = (f'```\n _________\n'
+                f'|/      _|_\n'
+                r"|      |'-'|"
+                f'\n'
+                r'|      /[Y]\ '
+                f'\n'
+                r'|       '
+                f'\n'
+                f'|_____```')
+
+    hangman5 = (f'```\n _________\n'
+                f'|/      _|_\n'
+                r"|      |'-'|"
+                f'\n'
+                r'|      /[Y]\ '
+                f'\n'
+                r'|       /'
+                f'\n'
+                f'|_____```')
+
+
+    if message.content.lower() == '>hangman':
+        await message.channel.send(hangman_full)
+        await message.channel.send(hangman1)
+        await message.channel.send(hangman2)
+        await message.channel.send(hangman3)
+        await message.channel.send(hangman4)
+        await message.channel.send(hangman5)
 
 
 client.run(TOKEN)
