6032ab118c84df4ffcda97cd0a63d6f9fd374394
lbcai <73155687+lbcai@users.noreply.github.com>
2020-12-31 22:47:49 -0800

    Can now detect if a single letter guess is correct (but can't fill in the blanks in the correct spot yet) or can detect if a whole word guess is correct. Has an issue where word is generated on startup, new word is generated on game creation, correct answer for game remains the initial word.

--- PlayFriend/bot.py
+++ PlayFriend/bot.py
@@ -1,6 +1,7 @@
 import os
 import random
 import discord
+from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
@@ -9,13 +10,15 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = Bot(command_prefix='>')
 game_dictionary = {}
 
-class Hangman:
+
+class Hangman(commands.Cog):
     """Documentation here"""
     from module_vars import hangman_vocab, hangman_list
 
     def __init__(self):
         self.hangman_image_counter = 0
         self.word = random.choice(self.hangman_vocab)
+        print(self.word)
         self.guess = '```'
         for i in range(0, len(self.word[0])):
             if self.word[0][i] != ' ':
@@ -29,11 +32,20 @@ class Hangman:
         await message.channel.send(self.hangman_list[self.hangman_image_counter])
         await message.channel.send(self.guess)
 
-    @bot.command(name='hm', help='Place a single letter after "hm" to make a guess.', pass_context=True)
-    async def hangman_check(self, ctx):
-        for letter in self.word[0]:
-            if ctx.message[-1] == letter:
-                print(self.word[0].index(letter))
+    @commands.command(name='hm', help='Place a single letter after "hm" to make a guess.')
+    async def hangman_check(self, ctx, *, message):
+        print(message.strip().lower())
+        print(self.word[0].lower())
+        if len(message.strip()) == 1:
+            for letter in range(0, len(self.word[0])):
+                if message[-1].lower() == self.word[0][letter].lower():
+                    print('right', letter)
+                else:
+                    print('wrong', letter)
+        else:
+            if self.word[0].lower() == message.strip().lower():
+                await ctx.message.channel.send('Winner winner chicken dinner')
+
 
 
 @bot.command(name='hmstart', help='Starts a game of hangman.')
@@ -41,7 +53,7 @@ async def hangman_start(message):
     game_dictionary[message.author] = Hangman()
     await Hangman.hangman_output(game_dictionary[message.author], message)
 
-#bot.add_cog(Hangman())
+bot.add_cog(Hangman())
 
 
 @bot.event
