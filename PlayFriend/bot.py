5da2917836dc34120f2dab4c109f35858cbca415
lbcai <73155687+lbcai@users.noreply.github.com>
2021-01-03 23:19:56 -0800

    Added some docs for hangman class and deleted dict key for game that is finished instead of assigning the key value 'None' when game is over. Also added option to quit the hangman game.

--- PlayFriend/bot.py
+++ PlayFriend/bot.py
@@ -12,7 +12,13 @@ game_dictionary = {}
 
 
 class Hangman(commands.Cog):
-    """Documentation here"""
+    """Allows users to play hangman. Use function hangman_start to create game instances.
+    Function hangman_custom allows a user to submit their own word and restarts the game with that word as self.word[0].
+    Function hangman_check is used to check if the user's guess of a single letter or a full word matches self.word[0].
+    Function hangman_wrong is used when a guess is incorrect. It adds a hangman body part to the gallows, and when
+        the man is complete, it causes the user to lose the game.
+    Function hangman_win is used when the user has successfully guessed the entire word and won the game.
+    Function hangman_output uses hangman_guess_filler to display the current state of the word and the man in chat."""
 
     from module_vars import hangman_vocab, hangman_list
 
@@ -46,7 +52,7 @@ class Hangman(commands.Cog):
                 else:
                     hangman_custom_word += item
             await ctx.message.delete()
-            if game_dictionary[ctx.message.channel] is not None:
+            if ctx.message.channel in game_dictionary:
                 game_dictionary[ctx.message.channel].hangman_image_counter = 0
                 game_dictionary[ctx.message.channel].word = (hangman_custom_word, '...google it')
                 Hangman.hangman_guess_list(game_dictionary[ctx.message.channel])
@@ -66,13 +72,13 @@ class Hangman(commands.Cog):
             bot_message = f'Your man is hung. RIP. The word was {game_dictionary[ctx.message.channel].word[0].lower()}, ' \
                           f'which means {game_dictionary[ctx.message.channel].word[1]}.'
             await ctx.message.channel.send(bot_message)
-            game_dictionary[ctx.message.channel] = None
+            game_dictionary.pop(ctx.message.channel, None)
 
     async def hangman_win(self, ctx):
         bot_message = f'Congratulations! You won. The word was {game_dictionary[ctx.message.channel].word[0].lower()}, ' \
                       f'which means {game_dictionary[ctx.message.channel].word[1]}.'
         await ctx.message.channel.send(bot_message)
-        game_dictionary[ctx.message.channel] = None
+        game_dictionary.pop(ctx.message.channel, None)
 
     @commands.command(name='hm', help='Make a guess. Use single letters unless you are confident you know the entire word!')
     async def hangman_check(self, ctx, message):
@@ -102,11 +108,16 @@ class Hangman(commands.Cog):
             if '_' not in game_dictionary[ctx.message.channel].guess_list:
                 await Hangman.hangman_win(game_dictionary[ctx.message.channel], ctx)
 
+    @commands.command(name='hmquit', help='Quit the current hangman game.')
+    async def hangman_quit(self, ctx):
+        game_dictionary.pop(ctx.message.channel, None)
+        await ctx.message.channel.send('The hangman game has ended.')
+
 
 
 @bot.command(name='hmstart', help='Starts a game of hangman.')
 async def hangman_start(message):
-    if message.channel not in game_dictionary or game_dictionary[message.channel] is None:
+    if message.channel not in game_dictionary:
         game_dictionary[message.channel] = Hangman()
         await Hangman.hangman_output(game_dictionary[message.channel], message)
     else:
